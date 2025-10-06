fcadc19f66d9be3c60468387de4e92beb280ec6c
radykhovska <zagnibeda@i.ua>
2025-10-06 15:29:27 +0300

    filter added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState, useEffect } from "react";
 import Search from "./components/Search/Search";
 import Card from "./components/Card/Card";
 import Pagination from "./components/Pagination/Pagination";
-// import Filter from "./components/Filter/Filter";
+import { Filter } from "./components/Filter/Filter";
 // import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -15,8 +15,11 @@ function App() {
   let { info, results } = fetchedData;
   let [pageNumber, updatePageNumber] = useState(1);
   let [search, setSearch] = useState("");
+  let [status, updateStatus] = useState("");
+  let [gender, updateGender] = useState("");
+  let [species, updateSpecies] = useState("");
 
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
 
   useEffect(() => {
     (async function () {
@@ -32,7 +35,14 @@ function App() {
 
       <div className="container">
         <div className="row">
-          Filter component will be placed here
+          <Filter
+            pageNumber={pageNumber}
+            status={status}
+            updateStatus={updateStatus}
+            updateGender={updateGender}
+            updateSpecies={updateSpecies}
+            updatePageNumber={updatePageNumber}
+          />
           <div className="col-lg-8 col-12">
             <div className="row">
               <Card results={results} />
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders page title', () => {
+test("renders page title", () => {
   render(<App />);
-  expect(screen.getByRole('heading', { name: /characters/i })).toBeInTheDocument();
+  expect(
+    screen.getByRole("heading", { name: /characters/i })
+  ).toBeInTheDocument();
 });
